211d1fc9495d0fd0115e46a0c4b24964385b5295
alellouc <alellouc@student.42nice.fr>
2021-06-16 15:12:20 +0200

    Experiment va_list models

--- libftprintf.h
+++ libftprintf.h
@@ -6,13 +6,14 @@
 /*   By: alellouc <alellouc@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 12:44:16 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/13 12:45:18 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/16 14:24:19 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 # include "./libft/libft.h"
+# include <stdarg.h>
 
 int	ft_printf(const char *fmt, ...);
 #endif
