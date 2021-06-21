f5ebf2b314d2e02c1968c0a9569ec9c420ccd79f
AudeizReading <aaericia@hotmail.fr>
2021-06-21 22:30:01 +0200

    Doesn't work with a simple string, don't know, perhaps it is about va_args, I don't understand this behavior because when there are flags in ft_printf, for the moment that works...

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 08:12:37 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/21 14:22:15 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/21 22:24:55 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,28 +49,52 @@ int	ft_is_indicator(int c)
 	return (1);
 }
 
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long int	l_nbr;
+	(void)base;
+
+	l_nbr = nbr;
+	if (l_nbr < 0)
+	{
+		ft_putchar_fd('-', 1);
+		l_nbr = -l_nbr;
+	}
+	if (l_nbr >= 10)
+		ft_putnbr_base(l_nbr / 10, base);
+	ft_putchar_fd(l_nbr % 10 + '0', 1);
+}
+
 int	ft_printf(const char *format, ...)
 {
+	/* Ne pas oublier de recuperer la taille des elements pour le retour final */
+	/* Ne pas oublier de gerer les eetoiles avec la width et la precision */
+	/* Attention, je ne sais pas pourquoi je ne lis pas une chaine sans
+	** formattage -> a resoudre */
+	int				res;
+	char			*p_format;
 	va_list			args;
 	void			*v_arg;
 	t_printf_flags	*flag;
 
+	p_format = (char *)format;
+	(void) res;
 	flag = &(t_printf_flags){0, 0, 0, 0, 0, 0, 0, 0};
 	va_start(args, format);
-	while (*format)
+	ft_putendl_fd((char *)p_format, 1);
+	while (*p_format)
 	{
 		/* Il faudrait également vérifier qu'il y a autant de % que d'arguments
 		** en bouclant sur va_arg, donc on ferait deux boucles sur les params
 		** variadiques */
-		if (*format == '%' && *(++format))
+	//	if (*p_format == '%' && *(++p_format))
+		if (*p_format == '%')
 		{
+			p_format++;
 			v_arg = va_arg(args, void *);
-		/*	if (*format == '%')*/ /* A placer avec les indicateurs de conversion */
-			/*	ft_putchar_fd('%', 1);
-			else*/ if (ft_is_attribute(*format))
-			while (ft_is_attribute(*format))
+			while (ft_is_attribute(*p_format))
 			{
-				/* if (*format == '0' || *format == '-')*/
+				/* if (*p_format == '0' || *p_format == '-')*/
 				/* Nous avons donc un attribut à traiter, gestion alignement et
 				** remplissage*/
 				/* Si 0 remplissage à gauche avec des 0 si d i o u x X, sinon
@@ -78,16 +102,16 @@ int	ft_printf(const char *format, ...)
 				/* Si - remplissage à droite avec des espaces sauf si n, prend
 				** la priorité si associé avec 0*/
 				/* Par défaut alignement à droite*/
+
 				/* Faire une condition que si - et 0 en meme temps alors - */
 				flag->has_attribute = 1;
-				flag->attribute = *format;
-				ft_putstr_fd(&flag->attribute, 1);
+				flag->attribute = *p_format;
+				/*ft_putstr_fd(&flag->attribute, 1);
 				ft_putstr_fd("Il y a un alignement à gerer\n", 1);
-				/*ft_putstr_fd((char *)v_arg, 1);*/
-				format++;
+				ft_putstr_fd((char *)v_arg, 1);*/
+				p_format++;
 			}
-			/*else if (*format != '0' || ft_isdigit(*format)*/
-			if (*format >= '1' && *format <= '9')
+			if (*p_format >= '1' && *p_format <= '9')
 			{
 				/* Nous avons donc une largeur de champ à gérer, attention il
 				** peut y avoir une étoile remplaçant cette valeur, il faut la
@@ -101,14 +125,13 @@ int	ft_printf(const char *format, ...)
 				/* Pas de troncature avec ce flag, si la largeur est inf à la
 				** taille de l'argument la largeur s'adapte à cette taille */
 				flag->has_field_width = 1;
-				flag->width = ft_atoi(format);
-				ft_putnbr_fd(flag->width, 1);
-			/*	ft_putstr_fd((char *)v_arg, 1);*/
-				ft_putstr_fd("Il y a une largeur de champ à gérer\n", 1);
-				format += ft_intlen(flag->width);
+				flag->width = ft_atoi(p_format);
+			/*	ft_putnbr_fd(flag->width, 1);
+				ft_putstr_fd((char *)v_arg, 1);
+				ft_putstr_fd("Il y a une largeur de champ à gérer\n", 1);*/
+				p_format += ft_intlen(flag->width);
 			}
-			/*else if (*format == '.' && (ft_isdigit(*(++format)))) */
-			/*else*/ if (*format == '.' && (ft_isdigit(*(++format)))) 
+			if (*p_format == '.' && (ft_isdigit(*(++p_format)))) 
 				/* Attention cette condition ne considere pas une precision de . qui est valide mais indique une precision 0 */
 			{
 				/* Nous avons une précision à gérer, une précision est tjs
@@ -118,40 +141,34 @@ int	ft_printf(const char *format, ...)
 				/* Si précision sur chaine de char associée à une largeur de
 				** champ affiche alors "précision" max char */
 				flag->has_precision = 1;
-				flag->precision = ft_atoi(format);
-				ft_putnbr_fd(flag->precision, 1);
-				ft_putstr_fd("Il y a une précision à gérer\n", 1);
-				/*ft_putstr_fd((char *)v_arg, 1);*/
-				/*format += ft_intlen(ft_atoi(format));*/
-				format += ft_intlen(flag->precision);
+				flag->precision = ft_atoi(p_format);
+			/*	ft_putnbr_fd(flag->precision, 1);
+				ft_putstr_fd("Il y a une précision à gérer\n", 1);*/
+				p_format += ft_intlen(flag->precision);
 			}
-			else if (ft_is_indicator(*format))
+			if (ft_is_indicator(*p_format))
 			{
-				/* if (*format == 'd' || *format == 'i' || *format == 'u' ||
-				 *format == 'x' || *format == 'X' || *format == 'c' ||
-				 *format == 's' || *format == 'p')*/
-				/* Nous avons donc un indicateur de conversion à gérer */
 				flag->has_indicator = 1;
-				flag->indicator = *format;
-				ft_putstr_fd("Il y a une conversion de l'argument courant à effectuer\n", 1);
-				ft_putstr_fd((char *)v_arg, 1);
+				flag->indicator = *p_format;
+			/*	ft_putstr_fd("Il y a une conversion de l'argument courant à effectuer\n", 1);*/
 				if (flag->indicator == '%')
-					ft_putchar_fd('%', 1);
+					ft_putchar_fd(*p_format, 1);
 				else if (flag->indicator == 'd' || flag->indicator == 'i')
 					ft_putnbr_fd((int)v_arg, 1);
 				else if (flag->indicator == 'u' || flag->indicator == 'x' || flag->indicator == 'X')
-					ft_putnbr_fd((unsigned int)v_arg, 1); /* ft_putnbr_base */
+					ft_putnbr_base((unsigned int)v_arg,"0123456789");
+					/*ft_putnbr_fd((unsigned int)v_arg, 1);  ft_putnbr_base */
 				else if (flag->indicator == 'c' || flag->indicator == 's')
 					ft_putstr_fd((char *)v_arg, 1);
 				else if (flag->indicator == 'p')
 					ft_putstr_fd((char *)v_arg, 1);
 			}
-			else
-				ft_putstr_fd((char *)v_arg, 1);
+			//else
+				//ft_putchar_fd((char)*p_format, 1);
 		}
 		else
-			ft_putchar_fd((char)*format, 1);
-		format++;
+			ft_putchar_fd((char)*p_format, 1);
+		p_format++;
 	}
 	va_end(args);
 	return (0);
