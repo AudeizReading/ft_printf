e108319faddc9f045a4748f056cb387951d5c86c
alellouc <alellouc@student.42nice.fr>
2021-06-23 16:44:37 +0200

    Working on the star withe the field width

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 08:12:37 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/23 11:30:06 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/23 16:39:06 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_is_attribute(int c)
 
 int ft_is_field_width(int c)
 {
-	if (c < '1' || c > '9')
+	if (c < '1' || c > '9'/* || c == '*'*/)
 		return (0);
 	return (1);
 }
@@ -69,26 +69,6 @@ int		ft_check_base(char *base, int *base_2_convert)
 	return (0);
 }
 
-
-/*void	ft_putnbr_base(int nbr, char *base)
-{
-	long int	l_nbr;
-	int			size_base;
-
-	l_nbr = nbr;
-	size_base = 0;
-	if (ft_check_base(base, &size_base))
-			return;
-	if (l_nbr < 0)
-	{
-		ft_putchar_fd('-', 1);
-		l_nbr = -l_nbr;
-	}
-	if (l_nbr >= size_base)
-		ft_putnbr_base(l_nbr / size_base, base);
-	ft_putchar_fd(base[l_nbr % size_base], 1);
-}*/
-
 int	ft_putnbr_base(int nbr, char *base, int restart)
 {
 	long int	l_nbr;
@@ -108,34 +88,33 @@ int	ft_putnbr_base(int nbr, char *base, int restart)
 		res++;
 	}
 	if (l_nbr >= size_base)
-	{
 		ft_putnbr_base(l_nbr / size_base, base, 0);
-	}
-//	ft_putchar_fd(base[l_nbr % size_base], 1);
 	res += ft_int_putchar_fd(base[l_nbr % size_base], 1);
 	return (res);
 }
 
-void	ft_luint_putnbr_base(unsigned int nbr, char *base)
+int	ft_luint_putnbr_base(unsigned int nbr, char *base, int restart)
 {
 	unsigned long int	l_nbr;
 	int					size_base;
+	static int			res = 0;
 
+	if (restart)
+		res = 0;
 	l_nbr = nbr;
 	size_base = 0;
 	if (ft_check_base(base, &size_base))
-			return;
+			return (-1);
 	if (l_nbr >= (unsigned int)size_base)
-		ft_luint_putnbr_base(l_nbr / size_base, base);
-	ft_putchar_fd(base[l_nbr % size_base], 1);
+		ft_luint_putnbr_base(l_nbr / size_base, base, 0);
+	res += ft_int_putchar_fd(base[l_nbr % size_base], 1);
+	return (res);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	/* Ne pas oublier de recuperer la taille des elements pour le retour final */
-	/* Ne pas oublier de gerer les eetoiles avec la width et la precision */
-	/* Attention, je ne sais pas pourquoi je ne lis pas une chaine sans
-	** formattage -> a resoudre */
+	/* Ne pas oublier de gerer les etoiles avec la width et la precision */
 	int				res;
 	char			*p_format;
 	va_list			args;
@@ -143,22 +122,15 @@ int	ft_printf(const char *format, ...)
 	t_printf_flags	*flag;
 
 	p_format = (char *)format;
-	if (!p_format)
-	{
-		ft_putendl_fd("(null)", 1);
-		return (0);
-	}
-	(void) res;
+	res = 0;
 	flag = &(t_printf_flags){0, 0, 0, 0, 0, 1, 0, 0};
 	va_start(args, format);
-	//ft_putendl_fd((char *)p_format, 1);
 	while (*p_format)
 	{
 		/* Il faudrait également vérifier qu'il y a autant de % que d'arguments
 		** en bouclant sur va_arg, donc on ferait deux boucles sur les params
 		** variadiques */
 		if (*p_format == '%' && *(++p_format))
-	//	if (*p_format == '%')
 		{
 			v_arg = va_arg(args, void *);
 			while (ft_is_attribute(*p_format))
@@ -176,7 +148,7 @@ int	ft_printf(const char *format, ...)
 				flag->attribute = *p_format;
 				p_format++;
 			}
-			if (ft_is_field_width(*p_format))
+			if (ft_is_field_width(*p_format) || *p_format == '*')
 			{
 				/* Nous avons donc une largeur de champ à gérer, attention il
 				** peut y avoir une étoile remplaçant cette valeur, il faut la
@@ -190,8 +162,17 @@ int	ft_printf(const char *format, ...)
 				/* Pas de troncature avec ce flag, si la largeur est inf à la
 				** taille de l'argument la largeur s'adapte à cette taille */
 				flag->has_field_width = 1;
-				flag->width = ft_atoi(p_format);
-				p_format += ft_intlen(flag->width);
+				if (*p_format == '*')
+				{
+					flag->width = (int)v_arg;
+					v_arg = va_arg(args, void *);
+					p_format++;
+				}
+				else
+				{
+					flag->width = ft_atoi(p_format);
+					p_format += ft_intlen(flag->width);
+				}
 			}
 			if (*p_format == '.') 
 			{
@@ -217,34 +198,30 @@ int	ft_printf(const char *format, ...)
 				flag->has_indicator = 1;
 				flag->indicator = *p_format;
 				if (flag->indicator == '%')
-				{
-					ft_putchar_fd(*p_format, 1);
-				}
+					res += ft_int_putchar_fd(*p_format, 1);
 				else if (flag->indicator == 'd' || flag->indicator == 'i')
-					ft_putnbr_base(ft_putnbr_base((unsigned int)v_arg,"0123456789", 1), "0123456789", 1);
+					res += ft_putnbr_base((unsigned int)v_arg,"0123456789", 1);
 				else if (flag->indicator == 'u' || flag->indicator == 'x' || flag->indicator == 'X')
 				{
 					if (flag->indicator == 'u')
-						ft_luint_putnbr_base((unsigned int)v_arg, "0123456789");
+						res += ft_luint_putnbr_base((unsigned int)v_arg, "0123456789", 1);
 					else if (flag->indicator == 'x')
-						ft_luint_putnbr_base((unsigned int)v_arg,"0123456789abcdef");
+						res += ft_luint_putnbr_base((unsigned int)v_arg,"0123456789abcdef", 1);
 					else
-						ft_luint_putnbr_base((unsigned int)v_arg,"0123456789ABCDEF");
+						res += ft_luint_putnbr_base((unsigned int)v_arg,"0123456789ABCDEF", 1);
 				}
 				else if (flag->indicator == 'c' || flag->indicator == 's')
+					// C'est ici qu'il faut gerer le (null)
 					ft_putstr_fd((char *)v_arg, 1);
 				else if (flag->indicator == 'p')
 					ft_putstr_fd((char *)v_arg, 1);
 			}
 		}
 		else
-			ft_putchar_fd((char)*p_format, 1);
+			res += ft_int_putchar_fd((char)*p_format, 1);
 		p_format++;
-		// If I don't put a \n here, if there is no \n at the end, there is no output, don't know why because on the wsl this matter doesn't exist
-		// It is stdout the problem -> This file is buffered before getting out its flow, so it is an expected behavior, thank you for having past one day and half on it...
-		// For testing send the output toward arbitrary file descriptor that it is not stdout and make a diff a the ft_printf file and the printf file
-		//ft_putchar_fd('\n', 1);
 	}
 	va_end(args);
-	return (0);
+	//ft_putnbr_fd(res, 1);
+	return (res);
 }
