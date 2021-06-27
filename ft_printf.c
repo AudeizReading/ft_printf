4e76d1c2e166dbd0e7b082c013890792bdedaa71
AudeizReading <aaericia@hotmail.fr>
2021-06-27 21:20:43 +0200

    Searching on format

--- ft_printf.c
+++ ft_printf.c
@@ -6,84 +6,11 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 08:12:37 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/27 14:19:31 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/27 15:27:26 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-/*
-int	ft_int_putchar_fd(char c, int fd)
-{
-	return (write(fd, &c, 1));
-}
-
-int	ft_int_putstr_fd(char *str, int fd)
-{
-	int	res;
-
-	res = 0;
-	while (*str)
-		res += ft_int_putchar_fd(*str++, fd);
-	return (res);
-}
-
-t_bool		ft_check_invalid_base(char *base, int *base_2_convert)
-{
-	while (base[*base_2_convert])
-	{
-		if (base[*base_2_convert] == base[*base_2_convert + 1]\
-				|| base[*base_2_convert] == 43 || base[*base_2_convert] == 45\
-				|| base[*base_2_convert] <= 32 || base[*base_2_convert] >= 126)
-			return (1);
-		(*base_2_convert)++;
-	}
-	if (*base_2_convert < 2)
-		return (true);
-	return (false);
-}
-
-int	ft_putnbr_base(int nbr, char *base, t_bool restart)
-{
-	long int	l_nbr;
-	static int	sum = 0;
-	int			size_base;
-
-	if (restart)
-		sum = 0;
-	l_nbr = nbr;
-	size_base = 0;
-	if (ft_check_invalid_base(base, &size_base))
-			return (-1);
-	if (l_nbr < 0)
-	{
-		ft_putchar_fd('-', 1);
-		l_nbr = -l_nbr;
-		sum++;
-	}
-	if (l_nbr >= size_base)
-		ft_putnbr_base(l_nbr / size_base, base, false);
-	sum += ft_int_putchar_fd(base[l_nbr % size_base], 1);
-	return (sum);
-}
-
-int	ft_luint_putnbr_base(unsigned long int nbr, char *base, t_bool restart)
-{
-	unsigned long int	l_nbr;
-	int					size_base;
-	static int			sum = 0;
-
-	if (restart)
-		sum = 0;
-	l_nbr = nbr;
-	size_base = 0;
-	if (ft_check_invalid_base(base, &size_base))
-			return (-1);
-	if (l_nbr >= (unsigned int)size_base)
-		ft_luint_putnbr_base(l_nbr / size_base, base, false);
-	sum += ft_int_putchar_fd(base[l_nbr % size_base], 1);
-	return (sum);
-}
-*/
 /*void	set_indicator(t_printf_flags **flag, char **p_format, int v_arg)
 {
 }*/
@@ -109,7 +36,8 @@ int	ft_printf(const char *format, ...)
 		** variadiques */
 		if (*p_format == '%' && *(++p_format))
 		{
-			v_arg = va_arg(args, void *);
+			if (/*!flag->indicator ||*/ flag->indicator != '%')
+				v_arg = va_arg(args, void *);
 			ft_set_attribute(&flag, &p_format);
 			ft_set_width(&flag, &p_format, (int)v_arg);
 			if (flag->has_star_width)
@@ -121,8 +49,16 @@ int	ft_printf(const char *format, ...)
 			{
 				flag->has_indicator = 1;
 				flag->indicator = *p_format;
+			//	ft_putstr_fd("flag->indicator: ", 1);
+			//	ft_putendl_fd(&flag->indicator, 1);
+			//	ft_putnbr_base((int)v_arg,"0123456789", true);
+			//	ft_putchar_fd('\n', 1);
 				if (flag->indicator == '%')
+				{
+					// Le pb est du a l'incrementation de args si %% avant %d
 					sum += ft_int_putchar_fd(*p_format, 1);
+				//	break;
+				}
 				else if (flag->indicator == 'd' || flag->indicator == 'i')
 					sum += ft_putnbr_base((int)v_arg,"0123456789", true);
 				else if (flag->indicator == 'u' || flag->indicator == 'x' || flag->indicator == 'X')
@@ -156,10 +92,5 @@ int	ft_printf(const char *format, ...)
 		p_format++;
 	}
 	va_end(args);
-	//ft_putnbr_fd(sum, 1);
-	//ft_putnbr_fd(ft_int_putstr_fd(" Test fn\n", 1), 1);
-	//ft_putstr_fd("Attribut : ", 1);
-	//ft_putstr_fd(&flag->attribute, 1);
-	//ft_putstr_fd("\n", 1);
 	return (sum);
 }
