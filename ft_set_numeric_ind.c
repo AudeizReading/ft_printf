72e58d02a12c03254bb076256b77fde9fd51e29b
alellouc <alellouc@student.42nice.fr>
2021-07-08 10:02:49 +0200

    adding last files of the project have forgotten them

---
+++ ft_set_numeric_ind.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_set_numeric_ind.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 13:52:34 by alellouc          #+#    #+#             */
+/*   Updated: 2021/07/07 13:52:56 by alellouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	ft_set_ind_d(va_list args)
+{
+	int		v_arg;
+	int		sum;
+
+	sum = 0;
+	v_arg = va_arg(args, int);
+	return (sum += ft_putnbr_base(v_arg, "0123456789", true));
+}
+
+int	ft_set_ind_i(va_list args)
+{
+	int		v_arg;
+	int		sum;
+
+	sum = 0;
+	v_arg = va_arg(args, int);
+	return (sum += ft_putnbr_base(v_arg, "0123456789", true));
+}
+
+int	ft_set_ind_u(va_list args)
+{
+	unsigned int	v_arg;
+	int				sum;
+
+	sum = 0;
+	v_arg = va_arg(args, unsigned int);
+	return (sum += ft_luint_putnbr_base(v_arg, "0123456789", true));
+}
+
+int	ft_set_ind_x(va_list args)
+{
+	unsigned int	v_arg;
+	int				sum;
+
+	sum = 0;
+	v_arg = va_arg(args, unsigned int);
+	return (sum += ft_luint_putnbr_base(v_arg, "0123456789abcdef", true));
+}
+
+int	ft_set_ind_X(va_list args)
+{
+	unsigned int	v_arg;
+	int				sum;
+
+	sum = 0;
+	v_arg = va_arg(args, unsigned int);
+	return (sum += ft_luint_putnbr_base(v_arg, "0123456789ABCDEF", true));
+}
---
+++ ft_set_other_ind.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_set_other_ind.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 14:23:05 by alellouc          #+#    #+#             */
+/*   Updated: 2021/07/07 14:43:00 by alellouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	ft_set_ind_c(va_list args)
+{
+	int	v_arg;
+	int	sum;
+
+	sum = 0;
+	v_arg = va_arg(args, int);
+	return (sum += ft_int_putchar_fd(v_arg, 1));
+}
+
+int	ft_set_ind_s(va_list args)
+{
+	char	*v_arg;
+	int		sum;
+
+	sum = 0;
+	v_arg = va_arg(args, char *);
+	if (!v_arg)
+		return (sum += ft_int_putstr_fd("(null)", 1));
+	else
+		return (sum += ft_int_putstr_fd(v_arg, 1));
+}
+
+int	ft_set_ind_p(va_list args)
+{
+	unsigned long int	*v_arg;
+	int					sum;
+
+	sum = 0;
+	v_arg = va_arg(args, unsigned long int *);
+	sum += ft_int_putstr_fd("0x", 1);
+	return (sum += ft_luint_putnbr_base((*v_arg), "0123456789abcdef", true));
+}
+
+int	ft_set_ind_percent(va_list args)
+{
+	int	sum;
+
+	(void) args;
+	sum = 0;
+	return (sum += ft_int_putchar_fd('%', 1));
+}
